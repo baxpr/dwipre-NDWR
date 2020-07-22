3c1a50c77ef9427c7fbbbe970ef0d2c006d782dd
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-07-22 18:15:23 -0500

    acqpar

--- test_simg.sh
+++ test_simg.sh
@@ -9,7 +9,7 @@ baxpr-dwipre-NDWR-master-v1.0.0.simg \
 --dwi_bvals /INPUTS/dwi.bval \
 --dwi_bvecs /INPUTS/dwi.bvec \
 --bet_opts "-f 0.3 -R" \
---acq_params "0 -1 0 0.69" \
+--acq_params "0 -1 0 0.08" \
 --project TESTPROJ \
 --subject TESTSUBJ \
 --session TESTSESS \
