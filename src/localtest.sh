f2e44b4062e55f296897f1a0b18ebada2fcb850e
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-07-30 15:51:40 -0500

    b0 threshold

--- src/localtest.sh
+++ src/localtest.sh
@@ -8,6 +8,7 @@ pipeline.sh \
     --dwi_bvecs ../INPUTS/dwi.bvec \
     --bet_opts "-f 0.3 -R" \
     --acq_params "0 -1 0 0.05" \
+    --b0_thresh 10 \
     --project TESTPROJ \
     --subject TESTSUBJ \
     --session TESTSESS \
